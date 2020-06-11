d24cc85155f56373d235e691fc6027a1d9c40d1a
Berthier <aberthier15@gmail.com>
2020-06-11 16:22:43 +0200

    fix

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from logging.config import dictConfig
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql:///app'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql:///app'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     dictConfig({
