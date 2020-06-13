ba7aa8c8fd5f9794ef9974746dcc57f5da70b3f7
Berthier <aberthier15@gmail.com>
2020-06-13 15:44:10 +0200

    Update login test for coverage

--- app/test/resources/user_test.py
+++ app/test/resources/user_test.py
@@ -49,7 +49,53 @@ def test_login(setup_app):
     assert 200 == response.status_code
 
 
+def test_login_error_no_data(setup_app):
+    application = setup_app["client_test"]
+
+    response = application.get('/user/login')
+
+    assert 400 == response.status_code
+
+
+def test_login_error_password(setup_app):
+    application = setup_app["client_test"]
+
+    username = "antoine_test"
+    password = "password_error"
+
+    payload = json.dumps({
+        "username": username,
+        "password": password
+    })
+
+    response = application.get('/user/login', headers={"Content-Type": "application/json"},
+                               data=payload)
+
+    assert 403 == response.status_code
+    assert "ERROR : user or login incorrect" == response.json["response"]
+
+
+def test_login_error_username(setup_app):
+    application = setup_app["client_test"]
+
+    username = "username_error"
+    password = "azerty"
+
+    payload = json.dumps({
+        "username": username,
+        "password": password
+    })
+
+    response = application.get('/user/login', headers={"Content-Type": "application/json"},
+                               data=payload)
+
+    assert 403 == response.status_code
+    assert "ERROR : user or login incorrect" == response.json["response"]
+
+
 def test_create_event(setup_app):
+    application = setup_app["client_test"]
+
     username = "antoine_test"
     password = "azerty"
 
@@ -58,8 +104,8 @@ def test_create_event(setup_app):
         "password": password
     })
 
-    setup_app["client_test"].get('/user/login', headers={"Content-Type": "application/json"},
-                                 data=payload)
+    application.get('/user/login', headers={"Content-Type": "application/json"},
+                    data=payload)
 
     titre_event = "Enenement de Test"
     date_event = "11/08/2020 12:30"
@@ -79,33 +125,9 @@ def test_create_event(setup_app):
     assert 200 == response.status_code
 
 
-def test_create_evenement(setup_app):
-    username = "antoine_test"
-    password = "azerty"
-
-    payload = json.dumps({
-        "username": username,
-        "password": password
-    })
-
-    setup_app["client_test"].get('/user/login', headers={"Content-Type": "application/json"},
-                                 data=payload)
-
-    titre_event = "Enenement de Test"
-    date_event = "11/08/2020 12:30"
-    description = "Test de creation event"
-
-    payload = json.dumps({
-        "titre": titre_event,
-        "description": description,
-        "date": date_event
-    })
-
-    setup_app["client_test"].post('/user/evenement', headers={"Content-Type": "application/json"},
-                                  data=payload)
-
-
 def test_get_event(setup_app):
+    application = setup_app["client_test"]
+
     username = "antoine_test"
     password = "azerty"
 
@@ -118,10 +140,10 @@ def test_get_event(setup_app):
     date_event = '2020-08-11 12:30:00'
     description = "Test de creation event"
 
-    setup_app["client_test"].get('/user/login', headers={"Content-Type": "application/json"},
-                                 data=payload)
+    application.get('/user/login', headers={"Content-Type": "application/json"},
+                    data=payload)
 
-    response = setup_app["client_test"].get('/user/evenement')
+    response = application.get('/user/evenement')
 
     assert dict == type(response.json['evenements'])
 
